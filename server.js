9ef2790c7eb3a8161c9290b895bad1fb63c3ae2e
Adam Mescher <admescher@gmail.com>
2018-01-02 11:26:12 -0700

    Add routes to server.js

--- server.js
+++ server.js
@@ -24,6 +24,55 @@ app.set('port', process.env.PORT || 4000);
 
 app.locals.title = 'APP NAME';
 
+app.get('/api/v1/sites', (request, response) => {
+  database('sites').select()
+  .then((response) => response.status(200).json({ sites }))
+  .catch(error => response.status(500).json({ error }));
+});
+
+app.get('/api/v1/sites/:id', (request, response) => {
+  const { id } = request.body;
+  database('sites').where('id', id).select()
+  .then((site) => {
+    if(!site.length){
+      return response.status(404).json({ error: `No site found with id ${id}` })
+    }
+    return response.status(200).json({ site });
+  })
+  .catch(error => response.status(500).json({ error }));
+});
+app.post('api/v1/sites', (request, response) => {})
+app.patch('api/v1/sites/:id', (request, response) => { });
+app.delete('api/v1/sites', (request, response) => { });
+
+app.post('api/v1/users', (request, response) => {});
+app.get('api/v1/users/:id', (request, response) => {});
+app.patch('api/v1/users/:id', (request, response) => {});
+app.delete('api/v1/users/:id', (request, response) => {});
+
+app.get('api/v1/users/:id/favorites', (request, response) => {});
+app.post('api/v1/users/:id/favorites', (request, response) => {});
+app.delete('api/v1/users/:id/favorites', (request, response) => {});
+
+app.get('api/v1/users/:id/visited', (request, response) => {});
+app.post('api/v1/users/:id/visited', (request, response) => {});
+app.delete('api/v1/users/:id/visited', (request, response) => {});
+
+app.get('api/v1/users/:id/wants', (request, response) => {});
+app.post('api/v1/users/:id/wants', (request, response) => {});
+app.delete('api/v1/users/:id/wants', (request, response) => {});
+
+app.use(function (request, response, next) {
+  response.status(404).send("Sorry can't find that!");
+  response.end();
+});
+
+app.use(function (error, request, response, next) {
+  console.error(error.stack);
+  response.status(500).send('Something broke!');
+  response.end();
+})
+
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
