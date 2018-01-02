7088a9bf06c3a251674988e0955952b63a87ceae
Alex Banister <alexbanister@gmail.com>
2018-01-02 13:36:50 -0700

    add user and site endpoints

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* eslint no-unused-vars: "off" */
 const express = require('express');
+const path = require('path');
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
@@ -17,12 +18,13 @@ const requireHTTPS = (request, response, next) => {
 };
 if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
 
-app.use(express.static('./build/'));
+const port = process.env.NODE_ENV === 'test' ? 5000 : process.env.PORT || 4000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('port', process.env.PORT || 4000);
+app.use(express.static('./build'));
+app.set('port', port);
 
-app.locals.title = 'APP NAME';
+app.locals.title = 'UNESCO Tracker';
 
 app.get('/api/v1/sites', (request, response) => {
   database('sites').select()
@@ -31,7 +33,7 @@ app.get('/api/v1/sites', (request, response) => {
 });
 
 app.get('/api/v1/sites/:id', (request, response) => {
-  const { id } = request.body;
+  const { id } = request.params;
   database('sites').where('id', id).select()
     .then((site) => {
       if (!site.length) {
@@ -41,26 +43,74 @@ app.get('/api/v1/sites/:id', (request, response) => {
     })
     .catch(error => response.status(500).json({ error }));
 });
-app.post('api/v1/sites', (request, response) => {});
-app.patch('api/v1/sites/:id', (request, response) => { });
-app.delete('api/v1/sites', (request, response) => { });
 
-app.post('api/v1/users', (request, response) => {});
-app.get('api/v1/users/:id', (request, response) => {});
-app.patch('api/v1/users/:id', (request, response) => {});
-app.delete('api/v1/users/:id', (request, response) => {});
+app.get('/api/v1/users/:id', (request, response) => {
+  const { id } = request.params;
+  database('users').where('id', id).select()
+    .then((user) => {
+      if (!user.length) {
+        return response.status(404).json({ error: `No user found with id ${id}` });
+      }
+      return response.status(200).json(user[0]);
+    })
+    .catch(error => response.status(500).json({ error }));
+});
+app.post('/api/v1/users', (request, response) => {
+  if (!request.body.email &&
+      !request.body.first_name &&
+      !request.body.last_name &&
+      !request.body.token) {
+    return response.status(422)
+      .json({ error: 'Expected format: { email: <String>, first_name: <String>, last_name: <String>, token: <String> }.' });
+  }
+  return database('users').insert(request.body, 'id')
+    .then((id) => {
+      response.status(201).json(id);
+    })
+    .catch((error) => {
+      response.status(500).json(error);
+    });
+});
+// app.delete('/api/v1/users/:id', (request, response) => {});
 
-app.get('api/v1/users/:id/favorites', (request, response) => {});
-app.post('api/v1/users/:id/favorites', (request, response) => {});
-app.delete('api/v1/users/:id/favorites', (request, response) => {});
+app.get('/api/v1/users/:id/favorites', (request, response) => {
+  const { userId } = request.params;
+  database('sites').where('id', userId).select()
+    .then((site) => {
+      if (!site.length) {
+        return response.status(404).json({ error: 'No favorites found' });
+      }
+      return response.status(200).json({ site });
+    })
+    .catch(error => response.status(500).json({ error }));
+});
+app.post('/api/v1/users/:id/favorites', (request, response) => {
+  const { id } = request.params;
+  const { siteId } = request.body;
+  if (!siteId) {
+    return response.status(422)
+      .json({ error: 'Expected format: { siteId: <Int> }.' });
+  }
+  return database('favorites').insert({
+    user_id: id,
+    site_id: siteId
+  })
+    .then(() => {
+      response.status(204).send();
+    })
+    .catch((error) => {
+      response.status(500).json(error);
+    });
+});
+app.delete('/api/v1/users/:id/favorites', (request, response) => {});
 
-app.get('api/v1/users/:id/visited', (request, response) => {});
-app.post('api/v1/users/:id/visited', (request, response) => {});
-app.delete('api/v1/users/:id/visited', (request, response) => {});
+app.get('/api/v1/users/:id/visited', (request, response) => {});
+app.post('/api/v1/users/:id/visited', (request, response) => {});
+app.delete('/api/v1/users/:id/visited', (request, response) => {});
 
-app.get('api/v1/users/:id/wants', (request, response) => {});
-app.post('api/v1/users/:id/wants', (request, response) => {});
-app.delete('api/v1/users/:id/wants', (request, response) => {});
+app.get('/api/v1/users/:id/wants', (request, response) => {});
+app.post('/api/v1/users/:id/wants', (request, response) => {});
+app.delete('/api/v1/users/:id/wants', (request, response) => {});
 
 app.use((request, response, next) => {
   response.status(404).send('Sorry can\'t find that!');
