f1317d37783b415a825b8ad393fe490acfb179b1
Adam Mescher <admescher@gmail.com>
2018-01-06 12:49:16 -0700

    Add stylesheet for Search and render component within SiteList

--- src/SiteList/index.js
+++ src/SiteList/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import Header from '../Header/';
+import Search from '../Search/';
 
 class SiteList extends Component {
   render() {
@@ -11,7 +12,11 @@ class SiteList extends Component {
         <section className='header__container'>
           <Header title={this.props.title}/>
         </section>
-        <section className='site-list__content'></section>
+        <section className='site-list__content'>
+          <article className='search-container'>
+            <Search />
+          </article>
+        </section>
       </div>
     );
   }
